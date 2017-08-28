4cfa2901afb04b81c46ead8c164b575fd7ff4572
Chris Sawarzynski <krzysztof.sawarzynski@protonmail.com>
2017-08-28 13:46:03 +0200

    change url for docker

--- walker/pipelines.py
+++ walker/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import requests
-EXPORT_URL = "http://localhost/search-engine/public/item"
+EXPORT_URL = "http://search-engine/item"
 
 class ExportPipeline(object):
     def process_item(self, item, spider):
